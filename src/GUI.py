d15618b74bacb317b89fb3a38def5316c2f2c92c
teobaillot <teo.baillot06@gmail.com>
2022-05-20 22:21:34 +0200

    ajout d'éléments à l'interface

--- src/GUI.py
+++ src/GUI.py
@@ -43,6 +43,14 @@ def main(game):
     screen.fill((255, 255, 255))
     screen.blit(pygame.transform.scale(pygame.image.load("assets/other/arcade.png"), (infoObject.current_w, infoObject.current_h)), (0, 0))
     start_button = Button((infoObject.current_w / 2) - 348 / 2, (infoObject.current_h / 2) + 149, pygame.image.load("assets/other/startButton.png"), 1)
+    font = pygame.font.SysFont(None, 20)
+    img = font.render('hello', True, (255,255,255))
+    img1 = font.render("strength:"+str(game.hero.strength), True, (255, 255, 255))
+    img2 = font.render("armor:"+str(game.hero.armor), True, (255, 255, 255))
+    #img3 = font.render(game.hero.exp, True, (255, 255, 255))
+    # img4 = font.render(game.hero.level, True, (255, 255, 255))
+    img5 = font.render("move:", True, (255, 255, 255))
+    x=0
     while 1:
         for event in pygame.event.get():
             if start_button.draw(screen):
@@ -52,7 +60,35 @@ def main(game):
             if event.type == pygame.QUIT:
                 sys.exit()
         if start_button.clicked:
-            screen.fill(black)
+            screen.fill((75,75,75))
+            pygame.draw.rect(screen, (64, 64, 64),pygame.Rect(tileSize * game.floor.size + 20, 20, infoObject.current_w - tileSize * game.floor.size - 40, infoObject.current_h - 40))
+            #boite de texte
+            pygame.draw.rect(screen, (55, 55, 55), pygame.Rect(infoObject.current_w * (3 / 5), infoObject.current_h / 2+80, tileSize*13, tileSize*2))
+            screen.blit(img, (infoObject.current_w * (3 / 5)+5, infoObject.current_h / 2+80))
+            #dessine le héro de l'inventaire
+            screen.blit(pygame.transform.scale(pygame.image.load("assets/hero/frontHero.png"), (tileSize*5, tileSize*5)), (infoObject.current_w*(4/5), 40))
+            #dessine les cases pour armes
+            pygame.draw.rect(screen, (55, 55, 55), pygame.Rect(infoObject.current_w*(4.65/5), 75, tileSize, tileSize))
+            pygame.draw.rect(screen, (55, 55, 55), pygame.Rect(infoObject.current_w * (4.65 / 5), 125, tileSize, tileSize))
+            #dessine les cases pour les armures
+            pygame.draw.rect(screen, (55, 55, 55), pygame.Rect(infoObject.current_w * (3.95 / 5), 40, tileSize, tileSize))
+            pygame.draw.rect(screen, (55, 55, 55), pygame.Rect(infoObject.current_w * (3.95 / 5), 90, tileSize, tileSize))
+            pygame.draw.rect(screen, (55, 55, 55), pygame.Rect(infoObject.current_w * (3.95 / 5), 140, tileSize, tileSize))
+            pygame.draw.rect(screen, (55, 55, 55), pygame.Rect(infoObject.current_w * (3.95 / 5), 190, tileSize, tileSize))
+            #caractéristiques du héro
+            screen.blit(img1, (infoObject.current_w*(4.2/5), 280))
+            screen.blit(img2, (infoObject.current_w*(4.2/5), 300))
+            #screen.blit(img3, (infoObject.current_w*(4/5), 220))
+            # screen.blit(img4, (infoObject.current_w*(4/5), 200))
+            #regles du jeu
+            screen.blit(img5, (infoObject.current_w * (3 / 5), infoObject.current_h *(8.5/10)))
+            screen.blit(pygame.transform.scale(pygame.image.load("assets/other/letterZ.png"), (tileSize*0.7, tileSize*0.7)), (infoObject.current_w * (3 / 5)+100, infoObject.current_h *(8.5/10)-40))
+            screen.blit(pygame.transform.scale(pygame.image.load("assets/other/letterQ.png"), (tileSize * 0.7, tileSize * 0.7)),
+                        (infoObject.current_w * (3 / 5) + 60, infoObject.current_h * (8.5 / 10) ))
+            screen.blit(pygame.transform.scale(pygame.image.load("assets/other/letterS.png"), (tileSize * 0.7, tileSize * 0.7)),
+                        (infoObject.current_w * (3 / 5) + 100, infoObject.current_h * (8.5 / 10)))
+            screen.blit(pygame.transform.scale(pygame.image.load("assets/other/letterD.png"), (tileSize * 0.7, tileSize * 0.7)),
+                        (infoObject.current_w * (3 / 5) + 140, infoObject.current_h * (8.5 / 10) ))
             for y in range(len(game.floor)):
                 for x in range(len(game.floor)):
                     e = game.floor.get(Coord(x, y))
@@ -62,6 +98,39 @@ def main(game):
                         screen.blit(pygame.transform.scale(pygame.image.load("assets/other/ground.png"), (tileSize, tileSize)), (x * tileSize, y * tileSize))
                         if e.image is not None:
                             screen.blit(pygame.transform.scale(pygame.image.load(e.image), (tileSize, tileSize)), (x * tileSize, y * tileSize))
+            for nbrElemMax in range(0,12):
+                pygame.draw.rect(screen, (55, 55, 55), pygame.Rect(tileSize * game.floor.size + 20 + x, infoObject.current_h/2, tileSize, tileSize))
+                x += 50
+            x = 0
+            for nbrElem in game.hero.inventory:
+                screen.blit(pygame.transform.scale(pygame.image.load(nbrElem.image), (tileSize, tileSize)), (tileSize * game.floor.size + 37 + x, infoObject.current_h/2))
+                x += 50
+            x = 0
+            xx = 0
+            y=infoObject.current_h / 20
+            for nbrHeartMax in range(0, 11):
+                if nbrHeartMax<6:
+                    screen.blit(pygame.transform.scale(pygame.image.load("assets/other/heartGrey.png"), (tileSize*0.5, tileSize*0.5)),
+                            (tileSize * game.floor.size + 37 + x, y))
+                    x += 50
+                else:
+                    y = infoObject.current_h / 20+50
+                    screen.blit(pygame.transform.scale(pygame.image.load("assets/other/heartGrey.png"), (tileSize*0.5, tileSize*0.5)),
+                            (tileSize * game.floor.size + 37 + xx, y))
+                    xx += 50
+            x = 0
+            y = infoObject.current_h / 20
+            xx = 0
+            for nbrHeart in range(game.hero.hp):
+                if nbrHeart<6:
+                    screen.blit(pygame.transform.scale(pygame.image.load("assets/other/heartRed.png"), (tileSize*0.5, tileSize*0.5)),
+                                (tileSize * game.floor.size + 37 + x, y))
+                    x += 50
+                else:
+                    x = 0
+                    y = infoObject.current_h / 20+50
+                    screen.blit(pygame.transform.scale(pygame.image.load("assets/other/heartRed.png"), (tileSize*0.5, tileSize*0.5)),
+                                (tileSize * game.floor.size + 37 + xx, y))
             printMsg(game)
             pygame.display.flip()
             if game.hero.hp <= 0:
