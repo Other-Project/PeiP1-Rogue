49e39a1e7e3b91b7971b60f7ccef3772c0c88f1d
EvanG <evan_g@orange.fr>
2022-06-17 18:16:10 +0200

    Oops, I forgot 2 lines ;)

--- src/Hero.py
+++ src/Hero.py
@@ -32,9 +32,9 @@ class Hero(Creature):
         self.all_projectiles = pygame.sprite.Group()
         self.invisible = 0
 
-    def shootProjectile(self, gui, monster):
+    def shootProjectile(self, gui, monster, onCollide=None):
         from Projectile import Projectile
-        self.all_projectiles.add(Projectile(gui, self, utils.theGame().floor.pos(monster)))
+        self.all_projectiles.add(Projectile(gui, self, utils.theGame().floor.pos(monster), onCollide))
 
     def description(self):
         return Creature.description(self) + str(self.inventory)
