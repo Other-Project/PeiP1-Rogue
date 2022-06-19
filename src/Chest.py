d9fe5c1bd91b6609f5b1f7ab490fbea81c8d9f2c
EvanG <evan_g@orange.fr>
2022-06-19 21:24:22 +0200

    If a safe has been opened, it remains opened after you exit the popup

--- src/Chest.py
+++ src/Chest.py
@@ -2,26 +2,29 @@ from Element import Element
 from    Item import Item
 
 
-
 class Chest(Element):
     key = Item("Key", image="assets/items/key.png")
-    def __init__(self, name: str = 'Chest', image="assets/items/chest.png", contain: list = None, size=3):
+
+    def __init__(self, name: str = 'Chest', image="assets/items/chest.png", items: list = None, size=3):
         import utils
         Element.__init__(self, name, image=image)
         self.size = size
-        self.items = contain or [utils.theGame().randEquipment() for _ in range(self.size)]
+        self.items = items or [utils.theGame().randEquipment() for _ in range(self.size)]
+        self.opened = False
 
     def meet(self, hero):
         import utils
         from Hero import Hero
         if isinstance(hero, Hero):
-            if self.key in hero.inventory:
-                utils.theGame().addMessage("You open the chest")
-                hero.inventory.remove(self.key)
+            if not self.opened:
+                if self.key in hero.inventory:
+                    utils.theGame().addMessage("You open the chest")
+                    self.opened = True
+                    hero.inventory.remove(self.key)
+                else:
+                    utils.theGame().addMessage("You don't have the key, kill monsters to find it")
+            if self.opened:
                 utils.theGame().gui.chestPopup(self, False)
-            else:
-                utils.theGame().addMessage("You don't have the key")
-                utils.theGame().addMessage("Kill Monster to find the key")
         return False
 
     def takeItem(self, hero, element):
@@ -31,5 +34,4 @@ class Chest(Element):
             self.items.remove(element)
         else:
             utils.theGame().addMessage("Your inventory is full")
-
         return True
